06511841d26fd0bcbc3474918c0da173ad2af632
Ajanthan <ajanthanasia@gmail.com>
2024-06-26 23:36:57 +0530

    channel

--- frontend/src/components/dashboard/channels/channels-index.jsx
+++ frontend/src/components/dashboard/channels/channels-index.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import Dashboard from "../dashboard";
-import { useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import axios from 'axios';
 
 function ChannelIndex() {
-    const [channels, setChannels] = useState([{ id: 1, name: 'cha' }]);
+    const navigate = useNavigate();
+    const [channels, setChannels] = useState([{ id: 1, channels: 'cha' }]);
     const loadUrl = 'http://localhost:4242/api/members/listChannels';
     const location = useLocation();
     const { id, name } = location.state || {};
@@ -12,19 +13,19 @@ function ChannelIndex() {
     const loadChannelsData = async (event) => {
         try {
             const response = await axios.get(`${loadUrl}`, {});
-            console.log(response);
+            setChannels(response.data);
         } catch (error) {
             console.log(error);
         }
     }
     setTimeout(() => {
         loadChannelsData();
-    }, 2000);
+    }, 1000);
     return (
         <div className="panel mt-1">
             <div className="row">
                 <div className="col-sm-3">
-                    <Dashboard id={id} name={name}/>
+                    <Dashboard id={id} name={name} />
                 </div>
                 <div className="col-sm-9">
                     <div className="row mb-1">
@@ -32,18 +33,21 @@ function ChannelIndex() {
                             List of Channels
                         </div>
                         <div className="col-md-6">
-                            <a href="channels-create">
-                                <button className="btn btn-primary form-control">
-                                    Create Channel
-                                </button>
-                            </a>
+                            <button className="btn btn-primary form-control"
+                                onClick={() => { navigate('/channels-create', { state: { id: id, name: name } }) }}>
+                                Create Channel
+                            </button>
                         </div>
                     </div>
                     <div className="row">
                         {channels.map((channel, index) => (
                             <div className="col-md-9" key={index}>
                                 <div className="form-control">
-                                    <span>{index}</span>
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <span>{channel.channels}</span>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         ))}
--- frontend/src/components/dashboard/channels/create.jsx
+++ frontend/src/components/dashboard/channels/create.jsx
@@ -1,33 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import Dashboard from "../dashboard";
+import { useNavigate, useLocation } from "react-router-dom";
+import axios from "axios";
 
 function CreateChannel() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { id, name } = location.state || {};
+    const [channel, setChannel] = useState("");
+    const url = 'http://localhost:4242/api/members/createchannel';
+    const successUrl = '/channels-index';
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post(`${url}`, {
+                userId: id,
+                channelName: channel,
+            });
+            console.log(response);
+            console.log(response.data);
+            console.log('Successfully updated');
+            const data = { id: id, name: name };
+            navigate(successUrl, { state: data });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div className="panel mt-1">
             <div className="row">
                 <div className="col-sm-3">
-                    <Dashboard />
+                    <Dashboard id={id} name={name} />
                 </div>
                 <div className="col-sm-9">
-                    <div className="row mb-1">
-                        <div className="col-md-10">
-                            Create New Channel
+                    <form onSubmit={handleSubmit}>
+                        <div className="row mb-1">
+                            <div className="col-md-10">
+                                Create New Channel
+                            </div>
+                            <div className="col-md-10">
+                                <strong>
+                                    <label htmlFor="channel">Channel name :</label>
+                                </strong>
+                                <input
+                                    type="text"
+                                    id="channel"
+                                    className="form-control"
+                                    value={channel}
+                                    onChange={(e) => setChannel(e.target.value)}
+                                />
+                            </div>
+                            <div className="col-md-10">
+                                <button type="submit" className="btn btn-primary form-control" >
+                                    Create Channel
+                                </button>
+                            </div>
                         </div>
-                        <div className="col-md-10">
-                            <strong>
-                                <label htmlFor="channel">Channel name :</label>
-                            </strong>
-                            <input
-                                type="text"
-                                id="channel"
-                                className="form-control" />
-                        </div>
-                        <div className="col-md-10">
-                            <button className="btn btn-primary form-control">
-                                Create Channel
-                            </button>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
